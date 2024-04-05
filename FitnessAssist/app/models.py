6fb1e391de7662be8ec925868e945dcf37f4fb8e
Alex Feldman <79878358+afeldmanSCSU@users.noreply.github.com>
2024-04-05 16:50:20 -0400

    Updated models.py

--- FitnessAssist/app/models.py
+++ FitnessAssist/app/models.py
@@ -5,3 +5,16 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(100))
+    gender = db.Column(db.String(10))
+    age = db.Column(db.Integer)
+    height = db.Column(db.Float)
+    weight = db.Column(db.Float)
+    activity_level = db.Column(db.String(20))
+    goal_type = db.Column(db.String(20))
+
+    user = db.relationship('User', backref=db.backref('profile', uselist=False))
